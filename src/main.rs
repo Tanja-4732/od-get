a3fc2c56c46bb3810910ab68584ff5fad1f9642b
Bernd-L <git@bernd.pw>
2021-01-03 19:57:15 +0100

    Print the license notice on regex error

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,13 @@ pub fn main() {
 
     let matches = cli::configure_parser(&pwd).get_matches();
 
-    let cli_options = cli::get_options(matches).expect(
-        "Invalid arguments (skip & limit must be numbers and the working directory must exist",
-    );
-
     // Print the name and version of the application along its license notice
     println!("{} {}", constants::NAME, constants::VERSION);
     println!("{}\n", constants::LICENSE);
+
+    let cli_options = cli::get_options(matches).expect(
+        "Invalid arguments (skip & limit must be numbers and the working directory must exist",
+    );
 
     // TODO do stuff
     println!("{:?}", cli_options);
