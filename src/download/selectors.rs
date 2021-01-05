2350c330a86d3ef412150532daa93d3f5d0057db
Bernd-L <git@bernd.pw>
2021-01-05 19:07:41 +0100

    TODO: Declare selectors once to avoid re-parsing them multiple times

---
+++ src/download/selectors.rs
@@ -0,0 +1,4 @@
+// use scraper::Selector;
+
+// TODO fix this to avoid re-parsing the same selectors multiple times
+// pub const DIRECTORY: &'static Selector = &Selector::parse("body > h1").unwrap();
