7d2b6f4acf720ece49463040f6acc33a94ae3e5a
Bernd-L <git@bernd.pw>
2021-02-01 16:02:27 +0100

    New concept

--- src/download/types.rs
+++ src/download/types.rs
@@ -1,4 +1,28 @@
+/**
+This enum defines an entry in an open-directory listing.
+
+- `Root` Represents the root URL of the open-directory listing
+- `File` Represents a link to a file to be downloaded
+- `PendingDir` Represents a link to a directory to be crawled
+- `CrawledDir` Represents a link to a crawled directory
+*/
 #[derive(Debug)]
+pub enum Node {
+    /// The root URL of the open directory listing
+    Root(String),
+
+    /// This node is a file
+    File(FileLinkMetaData),
+
+    /// This node is an un-crawled directory
+    PendingDir(DirLinkMetaData),
+
+    /// This node is a crawled directory
+    CrawledDir(Vec<Node>),
+}
+
+#[derive(Debug)]
+#[deprecated]
 pub struct DirData {
     pub path: String,
     pub files: Vec<FileLinkMetaData>,
